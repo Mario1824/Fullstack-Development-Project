28e19133d9ab9649cd51dc44abf82d80e72f9193
Mario Fuchu <marfch2@gmail.com>
2023-04-04 17:26:15 -0400

    Express website, static HTML only

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var path = require('path');
 var hbs = require('hbs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-
+var apiRouter = reuire('./app_api/routes/index');
 var app = express();
 
 // view engine setup
@@ -28,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
